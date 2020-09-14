43d72151e06212227b5ceb303823c6d30e7f76e7
unknown <jodramakombo96@gmail.com>
2020-09-14 11:22:14 +0100

    new js and new styling

---
+++ app.js
@@ -0,0 +1,14 @@
+var home = document.getElementById("hom");
+home.onmouseover = function () {
+  this.style.backgroundColor = "grey";
+};
+home.onmouseleave = function () {
+  this.style.backgroundColor = "";
+};
+var gall = document.getElementById("gal");
+gall.onmouseover = function () {
+  this.style.backgroundColor = "grey";
+};
+gall.onmouseleave = function () {
+  this.style.backgroundColor = "";
+};
